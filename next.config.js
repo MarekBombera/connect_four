5ff04884e1c5a4d223c627647567d09099584333
marek bombera <bomber.marek@gmail.com>
2022-12-03 17:04:54 +0100

    chore: initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    formats: ['image/webp'],
+  },
 }
 
 module.exports = nextConfig
