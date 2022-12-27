6990462fdf3b980cefc077742201aca8835579c0
marek bombera <bomber.marek@gmail.com>
2022-12-27 11:06:12 +0100

    feat: add turbopack + update nextconfig for turbopack

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  pageExtensions: ['page.tsx', 'page.ts'],
-  reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['typescript'],
+    transpilePackages: ['@acme/ui', 'lodash-es','styled-components'],
   },
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    formats: ['image/webp'],
-  },
+
+
 }
 
 module.exports = nextConfig
