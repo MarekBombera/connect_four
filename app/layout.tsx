f15c10fadfc7bdcbb0302539391871ac721ea4c3
marek bombera <bomber.marek@gmail.com>
2022-12-27 11:09:53 +0100

    feat: add base layout + registery for styled components

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import StyledComponentsRegistry from '~/features/ui/theme/registry'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
