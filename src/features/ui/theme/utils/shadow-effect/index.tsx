5154ac9dac4bb6e3e34fc2680344127185b30656
marek bombera <bomber.marek@gmail.com>
2022-12-28 21:37:40 +0100

    feat: add shadow-effect + style-reset

---
+++ src/features/ui/theme/utils/shadow-effect/index.tsx
@@ -0,0 +1,7 @@
+import { css } from 'styled-components'
+import { colors } from '../../colors.style'
+
+export const ShadowEffect = css`
+  border: 0.3rem solid ${colors.basic.black};
+  box-shadow: 0 1rem 0 ${colors.basic.black};
+`
---
+++ src/features/ui/theme/utils/style-reset/index.ts
@@ -0,0 +1,15 @@
+import { css } from 'styled-components'
+
+export const StyleReset = css`
+  width: auto;
+  height: auto;
+  padding: 0;
+  border: none;
+  font: inherit;
+  text-transform: inherit;
+  letter-spacing: inherit;
+  color: inherit;
+  appearance: none;
+  border-radius: 0;
+  background: none;
+`
