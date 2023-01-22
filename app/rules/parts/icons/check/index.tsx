1e141ab9324c997dbf40caba9864d2d31392270e
marek bombera <bomber.marek@gmail.com>
2023-01-22 18:09:28 +0100

    feat: add CheckIcon

---
+++ app/rules/parts/icons/check/index.tsx
@@ -0,0 +1,12 @@
+import { FC, SVGProps } from 'react'
+
+export const CheckIcon: FC = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width="34" height="24" viewBox="0 0 34 24" fill="none" {...props}>
+      <path
+        d="m31.0493.939331 2.1213 2.121319L12.264 23.9673.939331 12.6426l2.121319-2.1213L12.264 19.725 31.0493.939331Z"
+        fill="#fff"
+      />
+    </svg>
+  )
+}
