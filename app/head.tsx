8a24637f336dd27344a2f626b73c60a2886ec0ba
marek bombera <bomber.marek@gmail.com>
2023-01-22 15:18:47 +0100

    feat: add Head

---
+++ app/head.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+
+const Head: FC = () => {
+  return (
+    <head>
+      <title>Connect 4</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+  )
+}
+
+export default Head
