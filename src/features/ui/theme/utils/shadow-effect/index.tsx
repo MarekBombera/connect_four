32c785fc60846b063bad86350703ce46393c83ea
marek bombera <bomber.marek@gmail.com>
2023-01-22 09:57:31 +0100

    feat: add ShadowEffectButton

--- src/features/ui/theme/utils/shadow-effect/index.tsx
+++ src/features/ui/theme/utils/shadow-effect/index.tsx
@@ -5,6 +5,10 @@ import { colors } from '../../colors.style'
 
 export const ShadowEffect = css`
   border: 0.3rem solid ${colors.basic.black};
-  box-shadow: 0 1rem 0 ${colors.basic.black};
   border-radius: 2rem;
+  box-shadow: 0 1rem 0 ${colors.basic.black};
+`
+export const ShadowEffectButton = css`
+  border: 0.3rem solid ${colors.basic.black};
+  box-shadow: 0 0.5rem 0 ${colors.basic.black};
 `
