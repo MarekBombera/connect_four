5b84c8d0333a241def5dc102b16d7380a4789e42
marek bombera <bomber.marek@gmail.com>
2023-01-22 19:02:35 +0100

    feat: connect InGameMenu to GameNavigation

--- src/components/game-navigation/index.tsx
+++ src/components/game-navigation/index.tsx
@@ -1,13 +1,20 @@
+'use client'
+
 import { FC } from 'react'
 import { Logo } from '~/features/ui/components/logo'
+import { useModal } from '~/features/ui/components/modal/hooks/use-modal'
+import { InGameMenu } from '../in-game-menu'
 import { MenuButton, RestartButton, StyledGameNavigation } from './styled'
 
 type Props = {}
 
 export const GameNavigation: FC<Props> = ({}) => {
+  const { modalRef, isModalOpen, openModal, closeModal } = useModal({})
+
   return (
     <StyledGameNavigation>
-      <MenuButton>Menu</MenuButton>
+      {isModalOpen && <InGameMenu ref={modalRef} closeModal={closeModal} />}
+      <MenuButton onClick={openModal}>Menu</MenuButton>
       <Logo />
       <RestartButton>Restart</RestartButton>
     </StyledGameNavigation>
