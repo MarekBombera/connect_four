f6687a20651fdda469883c7d6bc6a2a97ab7b481
marek bombera <bomber.marek@gmail.com>
2023-01-22 09:59:30 +0100

    style: update GameNavigation style

---
+++ src/components/game-navigation/styled.ts
@@ -0,0 +1,41 @@
+'use client'
+
+import styled from 'styled-components'
+import { ButtonTemplate } from '~/features/ui/components/button/styled'
+import { colors } from '~/features/ui/theme/colors.style'
+import { mq } from '~/features/ui/theme/mq.style'
+import { typography } from '~/features/ui/theme/typography.style'
+import { ShadowEffectButton } from '~/features/ui/theme/utils/shadow-effect'
+
+export const StyledGameNavigation = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  justify-content: space-between;
+  align-items: center;
+  width: min(100%, 34rem);
+  margin: 0 auto;
+  padding: 5rem 0;
+
+  ${mq.medium} {
+    width: min(100%, 63.2rem);
+  }
+`
+
+const Button = styled(ButtonTemplate)`
+  justify-self: center;
+  padding: 0.8rem 1.9rem;
+  ${typography.label.medium}
+  text-transform: uppercase;
+  color: ${colors.basic.white};
+  background: ${colors.background.primary};
+  border: 0.3rem solid rgba(0, 0, 0, 0);
+  border-radius: 2rem;
+
+  &:hover {
+    ${ShadowEffectButton};
+    translate: 0 -0.2rem;
+    background: ${colors.accent.primary};
+  }
+`
+export const MenuButton = styled(Button)``
+export const RestartButton = styled(Button)``
