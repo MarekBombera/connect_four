7eb8845d6d63d84d0922934dee5da45964e7cd6f
marek bombera <bomber.marek@gmail.com>
2023-02-19 11:09:36 +0100

    refactor: improve rules

--- app/rules/constants.ts
+++ app/rules/constants.ts
@@ -1,8 +1,8 @@
 const rulesContent = {
-  1: 'Red goes first in the first game.',
-  2: 'Players must alternate turns, and only one disc can be dropped in each turn.',
-  3: 'The game ends when there is a 4-in-a-row or a stalemate.',
-  4: 'The starter of the previous game goes second on the next game.',
+  1: 'At the beginning of the game, the player with the red disc goes first. The other player will have the yellow disc.',
+  2: 'Players take turns dropping one disc at a time into the game board.',
+  3: 'The game ends when one player has four of their discs connected in a row, either vertically, horizontally, or diagonally. If all of the spaces on the board are filled without either player achieving four in a row, the game ends in a draw.',
+  4: 'After each game, the player who went second in the previous game will start the next game. This allows for equal opportunities for each player to start the game.',
 } as const
 
 export const rules = Object.values(rulesContent)
