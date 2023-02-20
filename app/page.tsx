a5a334b2a37d2097f72a4011863fa178db1eaa9a
marek bombera <bomber.marek@gmail.com>
2023-02-20 16:48:01 +0100

    feat: add Navigation to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { NextPage } from 'next'
+'use client'
 
+import { NextPage } from 'next'
 import { MainMenu } from './parts/main-menu'
 import { StyledHomePage } from './styled'
+import { Navigation } from '~/components/navigation'
 
 const HomePage: NextPage = () => {
   return (
     <StyledHomePage>
+      <Navigation />
       <MainMenu />
     </StyledHomePage>
   )
